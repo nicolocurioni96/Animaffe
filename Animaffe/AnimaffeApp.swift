af342c7ca301ed6b53c067083c4c200a0ba8f7dd
Nicolò Curioni <nicolo.curioni96@gmail.com>
2024-06-19 14:45:55 +0200

    Initial Commit

---
+++ Animaffe/AnimaffeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimaffeApp.swift
+//  Animaffe
+//
+//  Created by Nicolò Curioni on 19/06/24.
+//
+
+import SwiftUI
+
+@main
+struct AnimaffeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AnimaffeTests/AnimaffeTests.swift
@@ -0,0 +1,17 @@
+//
+//  AnimaffeTests.swift
+//  AnimaffeTests
+//
+//  Created by Nicolò Curioni on 19/06/24.
+//
+
+import Testing
+@testable import Animaffe
+
+struct AnimaffeTests {
+
+    @Test func testExample() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
